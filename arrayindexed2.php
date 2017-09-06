bd128eeaa8caa1140872cfba20bc7a02346276bf
unknown <windikadm3@gmail.com>
2017-09-06 13:47:10 +0700

    arrayexample

---
+++ arrayindexed2.php
@@ -0,0 +1,10 @@
+<?php
+
+$laptop = array ("asus", "accer", "toshiba", "lenovo", "apple");
+$arrlength = count($laptop);
+for($x =0; $x < $arrlength; $x++) {
+echo "besok saya punya laptop ";
+echo $laptop[$x] . " aamiin" . "<b>";
+echo "<br>";
+}
+?>
